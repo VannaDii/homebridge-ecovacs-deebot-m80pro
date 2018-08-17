fbe44a64543b37a15fa21b0e70ace2f104ffc072
Gio Palacino <gpalacino@staywell.com>
2018-08-17 12:38:57 -0500

    Updated path handling to ensure dirs.

--- src/ecovacs/apiClient.ts
+++ src/ecovacs/apiClient.ts
@@ -1,11 +1,11 @@
 import { Config } from './types';
 import { homedir } from 'os';
-import { writeFileSync, readFileSync, mkdirSync, existsSync } from 'fs';
+import { writeFileSync, readFileSync, mkdirSync, existsSync, PathLike } from 'fs';
 import { join, basename } from 'path';
 
 export class ApiClient {
   private _config: Config;
-  private _configPath: string = ApiClient.getConfigPath();
+  private _configPath: PathLike = ApiClient.getConfigPath();
 
   constructor() {
     this._config = ApiClient.readConfig(this._configPath);
@@ -20,7 +20,7 @@ export class ApiClient {
     ApiClient.saveConfig(this._configPath, this._config);
   }
 
-  private static readConfig(path: string): Config {
+  private static readConfig(path: PathLike): Config {
     if (!existsSync(path)) {
       return <Config>{};
     }
@@ -28,13 +28,18 @@ export class ApiClient {
     return <Config>JSON.parse(data);
   }
 
-  private static saveConfig(path: string, config: Config): void {
-    writeFileSync(path, JSON.stringify(config));
+  private static saveConfig(path: PathLike, config: Config): void {
+    writeFileSync(ApiClient.ensureDirs(path), JSON.stringify(config));
   }
 
-  private static getConfigPath(): string {
-    const path = join(homedir(), '.config', 'homebridge-ecovacs-deebot-m80pro.json');
-    const basePath = basename(path);
+  private static getConfigPath(): PathLike {
+    return ApiClient.ensureDirs(
+      join(homedir(), '.config', 'homebridge-ecovacs-deebot-m80pro.json')
+    );
+  }
+
+  private static ensureDirs(path: PathLike): PathLike {
+    const basePath = basename(path.toString());
     if (!existsSync(basePath)) {
       mkdirSync(basePath);
     }
